44486b0be027498a66763546289d9da721e08efd
kirti1602 <kirtikashyap9a@gmail.com>
2025-02-24 14:08:31 +0530

    Create q7.py

---
+++ q7.py
@@ -0,0 +1,6 @@
+# Get 4th element and 4th element from last
+my_tuple = (10, 20, 30, 40, 50, 60)
+fourth_element = my_tuple[3]  # 4th element (index starts at 0)
+fourth_from_last = my_tuple[-4]  # 4th element from last
+print(fourth_element)  # Output: 40
+print(fourth_from_last)  # Output: 30
