b6594dbe14e6c2decbc36471d28380811d540b41
kirti1602 <kirtikashyap9a@gmail.com>
2025-02-24 14:09:46 +0530

    Create q10.py

---
+++ q10.py
@@ -0,0 +1,7 @@
+# Check if an element exists in a tuple
+my_tuple = (1, 2, 3, 4, 5)
+element = 3
+if element in my_tuple:
+    print(f"{element} exists in the tuple.")
+else:
+    print(f"{element} does not exist in the tuple.")
